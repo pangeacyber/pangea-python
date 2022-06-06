42cb1c3c3cc4c701685a6acde3edf329fc9e7f71
Glenn Gallien <glenn.gallien@pangea.cloud>
2022-06-06 15:30:21 -0700

    Remove obsolete services and stray print. Add 'sources' search parameter. Treat 'old' and 'new' as JSON fields.

--- pangea/client.py
+++ pangea/client.py
@@ -1,4 +1,4 @@
-from pangea.services import Audit, Locate, Sanitize, Redact
+from pangea.services import Audit, Redact
 
 
 class PangeaClient(object):
@@ -7,8 +7,6 @@ class PangeaClient(object):
 
         self.__audit = None
         self.__redact = None
-        self.__locate = None
-        self.__sanitize = None
 
     @property
     def audit(self):
@@ -21,15 +19,3 @@ class PangeaClient(object):
         if not self.__redact:
             self.__redact = Redact(token=self.default_token)
         return self.__redact
-
-    @property
-    def locate(self):
-        if not self.__locate:
-            self.__locate = Locate(token=self.default_token)
-        return self.__locate
-
-    @property
-    def sanitize(self):
-        if not self.__sanitize:
-            self.__sanitize = Sanitize(token=self.default_token)
-        return self.__sanitize
--- pangea/services/__init__.py
+++ pangea/services/__init__.py
@@ -1,5 +1,3 @@
 from .audit import Audit
-from .locate import Locate
-from .sanitize import Sanitize
 from .tester import Tester
 from .redact import Redact
