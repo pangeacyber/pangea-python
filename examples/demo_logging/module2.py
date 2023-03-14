46759b2743a5952d3ec061022193fb2d600956a7
H. Andres Tournour <andres.tournour@pangea.cloud>
2023-03-14 11:47:20 -0300

    rename and delete deprecated

--- examples/demo_logging/module2.py
+++
@@ -1,8 +0,0 @@
-from pangea.audit_logger import AuditLogger, getLogger
-
-
-def foo():
-    logger: AuditLogger = getLogger(name=__name__)
-
-    logger.info("This is foo")
-    logger.audit("hello world")
