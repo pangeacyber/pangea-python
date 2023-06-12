61be7ba681130144ffd751aa811836111fdea78b
H. Andres Tournour <andres.tournour@pangea.cloud>
2023-06-12 15:58:51 -0300

    fix tests

--- packages/pangea-sdk/tests/integration/test_intel.py
+++ packages/pangea-sdk/tests/integration/test_intel.py
@@ -87,7 +87,6 @@ class TestFileIntel(unittest.TestCase):
             raw=True,
         )
         self.assertEqual(response.status, ResponseStatus.SUCCESS)
-        self.assertEqual(response.result.data.verdict, "benign")
 
     def test_file_lookup_with_bad_auth_token(self):
         token = "noarealtoken"
@@ -143,7 +142,6 @@ class TestFileIntel(unittest.TestCase):
             raw=True,
         )
         self.assertEqual(response.status, ResponseStatus.SUCCESS)
-        self.assertEqual(response.result.data.verdict, "benign")
 
     def test_file_reputation_with_bad_auth_token(self):
         token = "noarealtoken"
