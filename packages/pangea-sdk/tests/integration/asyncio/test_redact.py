c11c1ce0861abd5b455e3189267be3b7971d187d
H. Andrés Tournour <111303288+pangea-andrest@users.noreply.github.com>
2023-11-27 17:40:08 -0300

    Redact multiconfig support (#165)
    * add config id support
    * redact multiconfig tests
    * test against prod
    * asyncio implementation and test
    * typo

--- packages/pangea-sdk/tests/integration/asyncio/test_redact.py
+++ packages/pangea-sdk/tests/integration/asyncio/test_redact.py
@@ -5,7 +5,14 @@ import pydantic
 from pangea import PangeaConfig
 from pangea.asyncio.services.redact import RedactAsync
 from pangea.response import ResponseStatus
-from pangea.tools import TestEnvironment, get_test_domain, get_test_token, logger_set_pangea_config
+from pangea.tools import (
+    TestEnvironment,
+    get_config_id,
+    get_multi_config_test_token,
+    get_test_domain,
+    get_test_token,
+    logger_set_pangea_config,
+)
 
 TEST_ENVIRONMENT = TestEnvironment.LIVE
 
@@ -13,9 +20,10 @@ TEST_ENVIRONMENT = TestEnvironment.LIVE
 class TestRedact(unittest.IsolatedAsyncioTestCase):
     def setUp(self):
         token = get_test_token(TEST_ENVIRONMENT)
-        domain = get_test_domain(TEST_ENVIRONMENT)
-        config = PangeaConfig(domain=domain, custom_user_agent="sdk-test")
+        self.domain = get_test_domain(TEST_ENVIRONMENT)
+        config = PangeaConfig(domain=self.domain, custom_user_agent="sdk-test")
         self.redact = RedactAsync(token, config=config, logger_name="pangea")
+        self.multi_config_token = get_multi_config_test_token(TEST_ENVIRONMENT)
         logger_set_pangea_config(logger_name=self.redact.logger.name)
 
     async def asyncTearDown(self):
@@ -71,3 +79,43 @@ class TestRedact(unittest.IsolatedAsyncioTestCase):
 
         with self.assertRaises(pe.UnauthorizedException):
             await badredact.redact(text=text)
+
+    async def test_multi_config_redact(self):
+        config = PangeaConfig(domain=self.domain)
+        redact_multi_config = RedactAsync(self.multi_config_token, config=config)
+
+        with self.assertRaises(pe.PangeaAPIException):
+            text = "Jenny Jenny... 415-867-5309"
+            response = await redact_multi_config.redact(text=text)
+
+        await redact_multi_config.close()
+
+    async def test_multi_config_redact_config_1(self):
+        config_id = get_config_id(TEST_ENVIRONMENT, "redact", 1)
+        config = PangeaConfig(domain=self.domain)
+        redact_multi_config = RedactAsync(self.multi_config_token, config=config, config_id=config_id)
+
+        text = "Jenny Jenny... 415-867-5309"
+        expected = "<PERSON>... <PHONE_NUMBER>"
+
+        response = await redact_multi_config.redact(text=text)
+        self.assertEqual(response.status, ResponseStatus.SUCCESS)
+        self.assertEqual(response.result.redacted_text, expected)
+        self.assertEqual(response.result.count, 2)
+
+        await redact_multi_config.close()
+
+    async def test_multi_config_redact_config_2(self):
+        config_id = get_config_id(TEST_ENVIRONMENT, "redact", 2)
+        config = PangeaConfig(domain=self.domain)
+        redact_multi_config = RedactAsync(self.multi_config_token, config=config, config_id=config_id)
+
+        text = "Jenny Jenny... 415-867-5309"
+        expected = "<PERSON>... <PHONE_NUMBER>"
+
+        response = await redact_multi_config.redact(text=text)
+        self.assertEqual(response.status, ResponseStatus.SUCCESS)
+        self.assertEqual(response.result.redacted_text, expected)
+        self.assertEqual(response.result.count, 2)
+
+        await redact_multi_config.close()
