cf0d1992ba045eca31052ad78808afa35777d200
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2024-01-04 15:17:46 -0500

    fixup! leave asyncio tests disabled until Python v3.8
    These tests depend on `unittest.IsolatedAsyncioTestCase` which is not
    available in Python v3.7.

--- packages/pangea-sdk/tests/integration/asyncio/__init__.py
+++ packages/pangea-sdk/tests/integration/asyncio/__init__.py
@@ -1,6 +1,7 @@
-from .test_audit import TestAuditAsync  # noqa: F401
-from .test_authn import TestAuthN  # noqa: F401
-from .test_embargo import TestEmbargo  # noqa: F401
-from .test_intel import TestDomainIntel  # noqa: F401
-from .test_redact import TestRedact  # noqa: F401
-from .test_vault import TestVault  # noqa: F401
+# TODO: enable when CI runs on Python v3.8 or higher.
+# from .test_audit import TestAuditAsync  # noqa: F401
+# from .test_authn import TestAuthN  # noqa: F401
+# from .test_embargo import TestEmbargo  # noqa: F401
+# from .test_intel import TestDomainIntel, TestFileIntel, TestIPIntel, TestURLIntel, TestUserIntel  # noqa: F401
+# from .test_redact import TestRedact  # noqa: F401
+# from .test_vault import TestVault  # noqa: F401
