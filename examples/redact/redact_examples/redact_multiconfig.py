4cbfb3e08f4407ab66f574344e1b5de4b35c9b40
H. Andres Tournour <andres.tournour@pangea.cloud>
2024-01-05 15:55:07 -0300

    audit and redact multiconfig examples

---
+++ examples/redact/redact_examples/redact_multiconfig.py
@@ -0,0 +1,30 @@
+import os
+
+import pangea.exceptions as pe
+from pangea.config import PangeaConfig
+from pangea.services import Redact
+
+token = os.getenv("PANGEA_REDACT_MULTICONFIG_TOKEN")
+config_id = os.getenv("PANGEA_REDACT_CONFIG_ID")
+domain = os.getenv("PANGEA_DOMAIN")
+config = PangeaConfig(domain=domain)
+
+# Set config_id in service constructor
+redact = Redact(token, config=config, config_id=config_id)
+
+
+def main():
+    text = "Hello, my phone number is 123-456-7890"
+    print(f"Redacting PII from: {text}")
+
+    try:
+        redact_response = redact.redact(text=text)
+        print(f"Redacted text: {redact_response.result.redacted_text}")
+    except pe.PangeaAPIException as e:
+        print(f"Embargo Request Error: {e.response.summary}")
+        for err in e.errors:
+            print(f"\t{err.detail} \n")
+
+
+if __name__ == "__main__":
+    main()
