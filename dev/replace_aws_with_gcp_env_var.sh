2e11e2ba8f4e43ef26bf5d56a281170d71762c11
H. Andres Tournour <andres.tournour@pangea.cloud>
2024-01-17 16:28:27 -0300

    run script to use gcp variables

---
+++ dev/replace_aws_with_gcp_env_var.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script is to update all AWS (or default cloud provider) enviroment variables with GCP values
+# It's used to run tests against GCP
+
+# Iterate over all environment variables
+for var in $(env | cut -d= -f1); do
+    # Check if the variable ends with 'DEV', 'LVE', or 'STG'
+    if [[ $var == PANGEA*DEV || $var == PANGEA*LVE || $var == PANGEA*STG ]]; then
+        # Construct the name of the corresponding 'GCP' variable
+        gcp_var="${var}_GCP"
+
+        # Check if the 'GCP' variable is not set or empty
+        if [ -z "${!gcp_var}" ]; then
+            echo "Error: The 'GCP' variable for '$var' is not set or empty."
+            exit 1
+        fi
+
+        echo "Updating '$var'..."
+        # Replace the value of the current variable with the value of the 'GCP' variable
+        export "$var=${!gcp_var}"
+    fi
+done
