65a89aee4e508ac6fcad65665f430b5cd3e25256
H. Andres Tournour <andres.tournour@pangea.cloud>
2024-03-19 17:06:46 -0300

    AuthZ: Implementation, tests and docs

--- packages/pangea-sdk/pangea/services/__init__.py
+++ packages/pangea-sdk/pangea/services/__init__.py
@@ -1,5 +1,6 @@
 from .audit.audit import Audit
 from .authn.authn import AuthN
+from .authz import AuthZ
 from .embargo import Embargo
 from .file_scan import FileScan
 from .intel import DomainIntel, FileIntel, IpIntel, UrlIntel, UserIntel
