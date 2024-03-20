75611920e2d5a8c55e1885f1aa44d2b8635cc1bf
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2024-03-20 11:23:35 -0400

    pangea-sdk: add Sanitize docs (GEA-13292) (#319)

--- packages/pangea-sdk/pangea/services/sanitize.py
+++ packages/pangea-sdk/pangea/services/sanitize.py
@@ -4,31 +4,56 @@ import io
 from typing import Dict, List, Optional, Tuple
 
 from pangea.response import APIRequestModel, PangeaResponse, PangeaResponseResult, TransferMethod
+from pangea.services.base import ServiceBase
 from pangea.utils import FileUploadParams, get_file_upload_params
-
-from .base import ServiceBase
 
 
 class SanitizeFile(APIRequestModel):
     scan_provider: Optional[str] = None
+    """Provider to use for File Scan."""
+
     cdr_provider: Optional[str] = None
+    """Provider to use for CDR."""
 
 
 class SanitizeContent(APIRequestModel):
     url_intel: Optional[bool] = None
+    """Perform URL Intel lookup."""
+
     url_intel_provider: Optional[str] = None
+    """Provider to use for URL Intel."""
+
     domain_intel: Optional[bool] = None
+    """Perform Domain Intel lookup."""
+
     domain_intel_provider: Optional[str] = None
+    """Provider to use for Domain Intel lookup."""
+
     defang: Optional[bool] = None
+    """Defang external links."""
+
     defang_threshold: Optional[int] = None
+    """Defang risk threshold."""
+
     redact: Optional[bool] = None
+    """Redact sensitive content."""
+
     remove_attachments: Optional[bool] = None
+    """Remove file attachments (PDF only)."""
+
     remove_interactive: Optional[bool] = None
+    """Remove interactive content (PDF only)."""
 
 
 class SanitizeShareOutput(APIRequestModel):
     enabled: Optional[bool] = None
+    """Store Sanitized files to Pangea Secure Share."""
+
     output_folder: Optional[str] = None
+    """
+    Store Sanitized files to this Secure Share folder (will be auto-created if
+    it does not exist)
+    """
 
 
 class SanitizeRequest(APIRequestModel):
@@ -77,6 +102,21 @@ class SanitizeResult(PangeaResponseResult):
 
 
 class Sanitize(ServiceBase):
+    """Sanitize service client.
+
+    Examples:
+        import os
+
+        # Pangea SDK
+        from pangea.config import PangeaConfig
+        from pangea.services import Sanitize
+
+        PANGEA_SANITIZE_TOKEN = os.getenv("PANGEA_SANITIZE_TOKEN")
+        config = PangeaConfig(domain="pangea.cloud")
+
+        sanitize = Sanitize(token=PANGEA_SANITIZE_TOKEN, config=config)
+    """
+
     service_name = "sanitize"
 
     def sanitize(
@@ -95,6 +135,44 @@ class Sanitize(ServiceBase):
         uploaded_file_name: Optional[str] = None,
         sync_call: bool = True,
     ) -> PangeaResponse[SanitizeResult]:
+        """
+        Sanitize
+
+        Apply file sanitization actions according to specified rules. Beta API.
+
+        OperationId: sanitize_post_v1beta_sanitize
+
+        Args:
+            transfer_method: The transfer method used to upload the file data.
+            file_path: Path to file to sanitize.
+            file: File to sanitize.
+            source_url: A URL where the file to be sanitized can be downloaded.
+            share_id: A Pangea Secure Share ID where the file to be sanitized is stored.
+            file_scan: Options for File Scan.
+            content: Options for how the file should be sanitized.
+            share_output: Integration with Secure Share.
+            size: The size (in bytes) of the file. If the upload doesn't match, the call will fail.
+            crc32c: The CRC32C hash of the file data, which will be verified by the server if provided.
+            sha256: The hexadecimal-encoded SHA256 hash of the file data, which will be verified by the server if provided.
+            uploaded_file_name: Name of the user-uploaded file, required for `TransferMethod.PUT_URL` and `TransferMethod.POST_URL`.
+            sync_call: Whether or not to poll on HTTP/202.
+
+        Raises:
+            PangeaAPIException: If an API error happens.
+
+        Returns:
+            The sanitized file and information on the sanitization that was
+            performed.
+
+        Examples:
+            with open("/path/to/file.pdf", "rb") as f:
+                response = sanitize.sanitize(
+                    file=f,
+                    transfer_method=TransferMethod.POST_URL,
+                    uploaded_file_name="uploaded_file",
+                )
+        """
+
         if file or file_path:
             if file_path:
                 file = open(file_path, "rb")
@@ -139,6 +217,43 @@ class Sanitize(ServiceBase):
         sha256: Optional[str] = None,
         uploaded_file_name: Optional[str] = None,
     ) -> PangeaResponse[SanitizeResult]:
+        """
+        Sanitize via presigned URL
+
+        Apply file sanitization actions according to specified rules via a
+        [presigned URL](https://pangea.cloud/docs/api/presigned-urls). Beta API.
+
+        OperationId: sanitize_post_v1beta_sanitize 2
+
+        Args:
+            transfer_method: The transfer method used to upload the file data.
+            params: File upload parameters.
+            file_scan: Options for File Scan.
+            content: Options for how the file should be sanitized.
+            share_output: Integration with Secure Share.
+            size: The size (in bytes) of the file. If the upload doesn't match, the call will fail.
+            crc32c: The CRC32C hash of the file data, which will be verified by the server if provided.
+            sha256: The hexadecimal-encoded SHA256 hash of the file data, which will be verified by the server if provided.
+            uploaded_file_name: Name of the user-uploaded file, required for `TransferMethod.PUT_URL` and `TransferMethod.POST_URL`.
+
+        Raises:
+            PangeaAPIException: If an API error happens.
+
+        Returns:
+            A presigned URL.
+
+        Examples:
+            presignedUrl = sanitize.request_upload_url(
+                transfer_method=TransferMethod.PUT_URL,
+                uploaded_file_name="uploaded_file",
+            )
+
+            # Upload file to `presignedUrl.accepted_result.put_url`.
+
+            # Poll for Sanitize's result.
+            response: PangeaResponse[SanitizeResult] = sanitize.poll_result(response=presignedUrl)
+        """
+
         input = SanitizeRequest(
             transfer_method=transfer_method,
             file=file_scan,
