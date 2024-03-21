a2ca487c2c2529b0570a083490806d61ed88ae43
H. Andres Tournour <andres.tournour@pangea.cloud>
2024-03-21 19:00:20 -0300

    fixup! run isort

--- packages/pangea-sdk/pangea/asyncio/services/__init__.py
+++ packages/pangea-sdk/pangea/asyncio/services/__init__.py
@@ -4,6 +4,6 @@ from .embargo import EmbargoAsync
 from .file_scan import FileScanAsync
 from .intel import DomainIntelAsync, FileIntelAsync, IpIntelAsync, UrlIntelAsync, UserIntelAsync
 from .redact import RedactAsync
-from .share import ShareAsync
 from .sanitize import SanitizeAsync
+from .share import ShareAsync
 from .vault import VaultAsync
