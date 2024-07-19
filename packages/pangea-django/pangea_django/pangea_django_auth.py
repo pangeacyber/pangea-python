c24d895136964531c8202bfdc2e6b72d0e44e7cf
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2024-07-19 10:32:11 -0400

    pangea-django: add unit tests (PAN-13903)

--- packages/pangea-django/pangea_django/pangea_django_auth.py
+++ packages/pangea-django/pangea_django/pangea_django_auth.py
@@ -1,15 +1,17 @@
 # Copyright 2023 Pangea Cyber Corporation
 # Author: Pangea Cyber Corporation
+from __future__ import annotations
 
 import os
 import secrets
 import string
 import traceback
-from datetime import datetime
+from datetime import datetime, timezone
+from typing import Optional
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import BaseBackend
-from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth.models import AbstractBaseUser, AnonymousUser
 from django.http import HttpRequest
 from pangea.config import PangeaConfig
 from pangea.services.authn.authn import AuthN
@@ -43,19 +45,19 @@ class PangeaAuthMiddleware:
                 except ValueError:
                     # support older python that doesn't understand the trailing Z
                     expires = datetime.fromisoformat(expires.rstrip("Z"))
-                if expires < datetime.utcnow():
+                if expires < datetime.now(tz=timezone.utc):
                     refresh_token = request.session["PANGEA_REFRESH_TOKEN"]["token"]
-                    response = self.authn.client.refresh(refresh_token=refresh_token)
+                    response = self.authn.authn.client.session.refresh(refresh_token=refresh_token)
                     if not response.status == "Success":
                         raise self.InvalidSessionException
-                    active_token = response["active_token"]
-                    response.session["PANGEA_ACTIVE_TOKEN"] = active_token
+                    active_token = response.result.active_token
+                    request.session["PANGEA_ACTIVE_TOKEN"] = active_token.model_dump()
             else:
                 raise self.InvalidSessionException
         except self.InvalidSessionException as e:
             request.user = AnonymousUser()
         else:
-            request.user = self.authn.get_user(username=active_token["email"])
+            request.user = self.authn.get_user(username=active_token.email)
 
         return self.get_response(request)
 
@@ -68,7 +70,7 @@ class PangeaAuthentication(BaseBackend):
         self.authn = AuthN(token, config=self.config, logger_name="pangea")
         super().__init__()
 
-    def authenticate(self, request: HttpRequest):
+    def authenticate(self, request: HttpRequest) -> Optional[AbstractBaseUser]:  # type: ignore[override]
         code = request.GET.get("code")
         state = request.GET.get("state")
         expected_state = request.session.get("PANGEA_LOGIN_STATE")
@@ -78,21 +80,21 @@ class PangeaAuthentication(BaseBackend):
         resp = self.authn.client.userinfo(code=code)
         if resp and resp.status == "Success":
             try:
-                refresh = resp.raw_result["refresh_token"]
-                active = resp.raw_result["active_token"]
-                user, created = UserModel.objects.get_or_create(username=active["email"])
+                refresh = resp.result.refresh_token
+                active = resp.result.active_token
+                user, created = UserModel.objects.get_or_create(username=active.email)
                 if created:
-                    user.email = active["email"]
-                    user.first_name = active["profile"]["first_name"]
-                    user.last_name = active["profile"]["last_name"]
-                    user.last_login = active["profile"]["Last-Login-Time"]
+                    user.email = active.email  # type: ignore[attr-defined]
+                    user.first_name = active.profile.get("first_name", None)  # type: ignore[attr-defined]
+                    user.last_name = active.profile.get("last_name", None)  # type: ignore[attr-defined]
+                    user.last_login = active.profile.get("Last-Login-Time", None)
                     user.is_active = True
                     user.save()
                 else:
-                    user.last_login = active["profile"]["Last-Login-Time"]
+                    user.last_login = active.profile.get("Last-Login-Time", None)
                     user.save()
-                request.session["PANGEA_REFRESH_TOKEN"] = refresh
-                request.session["PANGEA_ACTIVE_TOKEN"] = active
+                request.session["PANGEA_REFRESH_TOKEN"] = refresh.model_dump()
+                request.session["PANGEA_ACTIVE_TOKEN"] = active.model_dump()
                 if "PANGEA_LOGIN_STATE" in request.session:
                     del request.session["PANGEA_LOGIN_STATE"]
             except Exception as e:
@@ -104,7 +106,7 @@ class PangeaAuthentication(BaseBackend):
         return user
 
     def logout(self, request: HttpRequest):
-        active = request.session.get("PANGEA_ACTIVE_TOKEN")
+        active: dict[str, str] = request.session.get("PANGEA_ACTIVE_TOKEN", {})
         token = active.get("token")
         if token:
             self.authn.client.session.logout(token)
