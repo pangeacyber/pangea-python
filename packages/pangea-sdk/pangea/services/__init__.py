c8f6495d0937abcb699f8bce6dc4e9deaa763fea
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2024-10-30 15:29:37 -0400

    pangea-sdk: rename Data Guard to AI Guard

--- packages/pangea-sdk/pangea/services/__init__.py
+++ packages/pangea-sdk/pangea/services/__init__.py
@@ -1,7 +1,7 @@
+from .ai_guard import AIGuard
 from .audit.audit import Audit
 from .authn.authn import AuthN
 from .authz import AuthZ
-from .data_guard import DataGuard
 from .embargo import Embargo
 from .file_scan import FileScan
 from .intel import DomainIntel, FileIntel, IpIntel, UrlIntel, UserIntel
--- packages/pangea-sdk/tests/integration/asyncio/test_ai_guard.py
+++ packages/pangea-sdk/tests/integration/asyncio/test_ai_guard.py
@@ -3,32 +3,32 @@ from __future__ import annotations
 import unittest
 
 from pangea import PangeaConfig
-from pangea.services import DataGuard
+from pangea.asyncio.services import AIGuardAsync
 from pangea.tools import TestEnvironment, get_test_domain, get_test_token, logger_set_pangea_config
 from tests.test_tools import load_test_environment
 
-TEST_ENVIRONMENT = load_test_environment(DataGuard.service_name, TestEnvironment.LIVE)
+TEST_ENVIRONMENT = load_test_environment(AIGuardAsync.service_name, TestEnvironment.LIVE)
 
 
-class TestDataGuard(unittest.TestCase):
+class TestAIGuardAsync(unittest.IsolatedAsyncioTestCase):
     def setUp(self) -> None:
         token = get_test_token(TEST_ENVIRONMENT)
         domain = get_test_domain(TEST_ENVIRONMENT)
         config = PangeaConfig(domain=domain, custom_user_agent="sdk-test")
-        self.client = DataGuard(token, config=config)
+        self.client = AIGuardAsync(token, config=config)
         logger_set_pangea_config(logger_name=self.client.logger.name)
 
-    def test_text_guard(self) -> None:
-        response = self.client.guard_text("hello world")
+    async def test_text_guard(self) -> None:
+        response = await self.client.guard_text("hello world")
         assert response.status == "Success"
         assert response.result
         assert response.result.redacted_prompt
-        assert response.result.findings.artifact_count == 0
-        assert response.result.findings.malicious_count == 0
+        assert response.result.findings.artifact_count is None
+        assert response.result.findings.malicious_count is None
 
-        response = self.client.guard_text("security@pangea.cloud")
+        response = await self.client.guard_text("security@pangea.cloud")
         assert response.status == "Success"
         assert response.result
         assert response.result.redacted_prompt
         assert response.result.findings.artifact_count == 1
-        assert response.result.findings.malicious_count == 0
+        assert response.result.findings.malicious_count is None
