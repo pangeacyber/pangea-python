60767784b6eaa0cf2defb798fe74d8477b565b33
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2025-01-27 14:17:06 -0500

    pangea-sdk: support for structured input to AI Guard

--- packages/pangea-sdk/tests/integration/asyncio/test_ai_guard.py
+++ packages/pangea-sdk/tests/integration/asyncio/test_ai_guard.py
@@ -18,15 +18,18 @@ class TestAIGuardAsync(unittest.IsolatedAsyncioTestCase):
         self.client = AIGuardAsync(token, config=config)
         logger_set_pangea_config(logger_name=self.client.logger.name)
 
+    async def asyncTearDown(self) -> None:
+        await self.client.close()
+
     async def test_text_guard(self) -> None:
-        response = await self.client.guard_text("hello world")
+        response = await self.client.guard_text("what was pangea?", debug=True)
         assert response.status == "Success"
         assert response.result
-        assert response.result.prompt
+        assert response.result.prompt_text
 
-        assert response.result.detectors.prompt_injection
-        assert response.result.detectors.prompt_injection.detected is False
-        assert response.result.detectors.prompt_injection.data is None
+        if response.result.detectors.prompt_injection:
+            assert response.result.detectors.prompt_injection.detected is False
+            assert response.result.detectors.prompt_injection.data is None
 
         assert response.result.detectors.pii_entity
         assert response.result.detectors.pii_entity.detected is False
@@ -39,9 +42,15 @@ class TestAIGuardAsync(unittest.IsolatedAsyncioTestCase):
         response = await self.client.guard_text("security@pangea.cloud")
         assert response.status == "Success"
         assert response.result
-        assert response.result.prompt
+        assert response.result.prompt_text
         assert response.result.detectors.pii_entity
         assert response.result.detectors.pii_entity.detected
         assert response.result.detectors.pii_entity.data
         assert response.result.detectors.pii_entity.data.entities
         assert len(response.result.detectors.pii_entity.data.entities) == 1
+
+    async def test_text_guard_structured(self) -> None:
+        response = await self.client.guard_text([{"role": "user", "content": "hello world"}])
+        assert response.status == "Success"
+        assert response.result
+        assert response.result.prompt_messages
