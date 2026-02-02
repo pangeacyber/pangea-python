f16199a9c3161be87ea9f426731898df59612d22
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2026-02-02 14:33:08 -0500

    pangea-sdk: remove Sanitize tests

--- packages/pangea-sdk/tests/integration/__init__.py
+++ packages/pangea-sdk/tests/integration/__init__.py
@@ -7,7 +7,6 @@ from .test_file_scan import TestFileScan
 from .test_intel import TestDomainIntel, TestFileIntel, TestIPIntel, TestURLIntel, TestUserIntel
 from .test_prompt_guard import TestPromptGuard
 from .test_redact import TestRedact
-from .test_sanitize import TestSanitize
 from .test_share import TestShare
 from .test_vault import TestVault
 
@@ -23,7 +22,6 @@ __all__ = (
     "TestIPIntel",
     "TestPromptGuard",
     "TestRedact",
-    "TestSanitize",
     "TestShare",
     "TestURLIntel",
     "TestUserIntel",
